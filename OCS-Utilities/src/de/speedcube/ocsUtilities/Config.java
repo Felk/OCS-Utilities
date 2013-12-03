b0c957c662ea06b7683923413328d6a821f8c834
Felk <studio.felk@gmail.com>
2013-12-03 21:15:31 +0100

    added registration

--- OCS-Utilities/src/de/speedcube/ocsUtilities/Config.java
+++ OCS-Utilities/src/de/speedcube/ocsUtilities/Config.java
@@ -3,5 +3,7 @@ package de.speedcube.ocsUtilities;
 public class Config {
 
 	public static final int SALT_LENGTH = 20;
+	public static final int USERNAME_MIN_LENGTH = 1;
+	public static final int USERNAME_MAX_LENGTH = 42;
 	
 }
--- OCS-Utilities/src/de/speedcube/ocsUtilities/language/LangAuthentification.java
+++ OCS-Utilities/src/de/speedcube/ocsUtilities/language/LangAuthentification.java
@@ -4,8 +4,9 @@ public class LangAuthentification {
 
 	// Registration
 	public static final int ERR_REG_USERNAME_TAKEN = 100;
-	public static final int ERR_REG_PASSWORD_INVALID = 101;
-	public static final int REGISTRATION_SUCCESS = 102;
+	public static final int ERR_REG_USERNAME_INVALID = 101;
+	public static final int ERR_REG_PASSWORD_INVALID = 102;
+	public static final int REGISTRATION_SUCCESS = 103;
 	
 	public static final int ERR_LOGIN_FAILED = 200;
 	public static final int LOGIN_SUCCESS = 201;
