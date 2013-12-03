359da1ab4332c3435cd54ef2f96f607fcc6b94fe
fkoen001 <fkoen001@PC13-104.intern.mi.hs-rm.de>
2013-12-03 13:32:20 +0100

    Working on registration, refactoring client information

--- OCS-Utilities/src/de/speedcube/ocsUtilities/Userranks.java
+++ OCS-Utilities/src/de/speedcube/ocsUtilities/Userranks.java
@@ -4,10 +4,11 @@ public class Userranks {
 
 	public static final int BANNED = -1;
 	public static final int NORMAL = 0;
-	public static final int HIGH = 1;
-	public static final int MOD = 2;
-	public static final int SMOD = 3;
-	public static final int ADMIN = 4;
-	public static final int DEV = 5;
+	public static final int GUEST = 1;
+	public static final int HIGH = 2;
+	public static final int MOD = 3;
+	public static final int SMOD = 4;
+	public static final int ADMIN = 5;
+	public static final int DEV = 6;
 
 }
