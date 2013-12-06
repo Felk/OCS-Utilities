3401311433ac58a23ea46d5824f0349738b2cba7
Nerogar <nerogar>
2013-12-06 19:29:12 +0100

    added PacketUserInfo

--- OCS-Utilities/src/de/speedcube/ocsUtilities/UserInfo.java
+++ OCS-Utilities/src/de/speedcube/ocsUtilities/UserInfo.java
@@ -1,5 +1,7 @@
 package de.speedcube.ocsUtilities;
 
+import de.speedcube.ocsUtilities.DNFile.DNFile;
+
 public class UserInfo {
 	public int userID;
 	public String username;
@@ -12,10 +14,35 @@ public class UserInfo {
 		color = 0xffffff;
 	}
 
-	public UserInfo(int userID, String username, int color, int rank) {
+	public UserInfo(int userID, String username, int rank, int color) {
 		this.userID = userID;
 		this.username = username;
 		this.color = color;
 		this.rank = rank;
+	}
+
+	public DNFile toDNFile() {
+		DNFile file = new DNFile("");
+		file.addNode("id", userID);
+		file.addNode("name", username);
+		file.addNode("col", color);
+		file.addNode("rank", rank);
+		return file;
+	}
+
+	public void setFromDNFile(DNFile file) {
+		userID = file.getInt("id");
+		username = file.getString("name");
+		color = file.getInt("col");
+		rank = file.getInt("rank");
+	}
+
+	public static UserInfo fromDNFile(DNFile file) {
+		UserInfo info = new UserInfo();
+		info.userID = file.getInt("id");
+		info.username = file.getString("name");
+		info.color = file.getInt("col");
+		info.rank = file.getInt("rank");
+		return info;
 	}
 }
---
+++ OCS-Utilities/src/de/speedcube/ocsUtilities/packets/PacketUserInfo.java
@@ -0,0 +1,59 @@
+package de.speedcube.ocsUtilities.packets;
+
+import java.util.ArrayList;
+
+import de.speedcube.ocsUtilities.UserInfo;
+import de.speedcube.ocsUtilities.DNFile.DNFile;
+
+public class PacketUserInfo extends Packet {
+
+	ArrayList<UserInfo> userInfos;
+
+	public PacketUserInfo() {
+		userInfos = new ArrayList<UserInfo>();
+	}
+
+	@Override
+	public void pack() {
+		data = new DNFile("");
+
+		int[] userIDs = new int[userInfos.size()];
+
+		for (int i = 0; i < userInfos.size(); i++) {
+			userIDs[i] = userInfos.get(i).userID;
+			data.addNode("b." + userIDs[i], userInfos.get(i).toDNFile().toByteArray());
+		}
+
+		data.addNode("a", userIDs);
+		packedData = data.toByteArray();
+	}
+
+	@Override
+	public void unpack() {
+		data = new DNFile("");
+		data.fromByteArray(packedData);
+
+		int[] userIDs = data.getIntArray("a");
+
+		for (int i = 0; i < userIDs.length; i++) {
+			DNFile file = new DNFile("");
+			file.fromByteArray(data.getByteArray("b." + userIDs[i]));
+			userInfos.add(UserInfo.fromDNFile(file));
+		}
+
+	}
+
+	public void addUserInfo(UserInfo userInfo) {
+		userInfos.add(userInfo);
+	}
+
+	public ArrayList<UserInfo> getUserInfos() {
+		return userInfos;
+	}
+
+	@Override
+	public String getName() {
+		return "Registration";
+	}
+
+}
