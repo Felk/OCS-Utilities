57eeff56d71a06b919b0b67ed3bd83aad5290236
Felk <studio.felk@gmail.com>
2013-12-09 01:35:03 +0100

    Added Config

--- OCS-Utilities/src/de/speedcube/ocsUtilities/Config.java
+++ OCS-Utilities/src/de/speedcube/ocsUtilities/Config.java
@@ -3,6 +3,6 @@ package de.speedcube.ocsUtilities;
 public class Config {
 
 	public static final int SALT_LENGTH = 20;
-	public static final String USERNAME_REGEXP = "^[a-zA-Z0-9הצü_.]{1,20}$";
+	public static final String USERNAME_REGEXP = "^[a-zA-Z0-9הצü_.\\-]{1,20}$";
 	
 }
