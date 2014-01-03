06a542b0e4023fa51c0829263f5012415c692f12
Felk <Felk@Felk-PC>
2014-01-03 19:27:31 +0100

    removed 1 unneccesary import

--- OCS-Utilities/src/de/speedcube/ocsUtilities/packets/PacketPartyData.java
+++ OCS-Utilities/src/de/speedcube/ocsUtilities/packets/PacketPartyData.java
@@ -1,7 +1,6 @@
 package de.speedcube.ocsUtilities.packets;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 import de.speedcube.ocsUtilities.PartyResultSet;
 import de.nerogar.DNFileSystem.DNFile;
