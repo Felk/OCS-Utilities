08842404e5a21c6aa04a61686a6e9771c26f736a
Felk <Felk@Felk-PC>
2014-01-03 21:08:52 +0100

    changed separator from char to String because Nerogar likes it better this way

--- OCS-Utilities/src/de/speedcube/ocsUtilities/Config.java
+++ OCS-Utilities/src/de/speedcube/ocsUtilities/Config.java
@@ -4,6 +4,6 @@ public class Config {
 
 	public static final int SALT_LENGTH = 20;
 	public static final String USERNAME_REGEXP = "^[a-zA-Z0-9הצü_.\\-]{1,20}$";
-	public static final char CHAT_CHANNEL_SEPARATOR = 0xffff;
-	
+	public static final String CHAT_CHANNEL_SEPARATOR = "" + ((char) 0xffff);
+
 }
